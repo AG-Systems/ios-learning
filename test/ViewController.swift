33943b07f9c131f329fb7613648d52a84f9625bd
maxadorable <root1@maxadorables-MacBook-Pro.local>
2017-10-26 13:26:34 -0700

    Started learning ios dev

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate  {
+    //MARK: Properties
+    @IBOutlet weak var title_text: UILabel!
+    
+    @IBOutlet weak var input: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Handle the text field’s user input through delegate callbacks.
+        input.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        title_text.text = textField.text
+    }
+    //MARK: Actions
+    @IBAction func actionkey(_ sender: UIButton)
+    {
+        title_text.text = "auriga"
+    }
+    
 }
 
